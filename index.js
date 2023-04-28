fe72c878b02234663daa2041bad4529355f3dbbc
mjavedahmed <44115120+mjavedahmed@users.noreply.github.com>
2023-04-29 01:44:19 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const http = require('http');
+const cron = require("node-cron")
+const axios = require("axios")
+
+console.log("Server Started")
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
@@ -10,3 +14,16 @@ const server = http.createServer((req, res) => {
 server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
 });
+async function fetchData() {
+  try {
+    const response = await axios.get("https://rilli-server.onrender.com")
+    console.log("Request successful:", response.data)
+  } catch (error) {
+    console.error("Error making request:", error.message)
+  }
+}
+
+cron.schedule("*/10 * * * *", function () {
+  fetchData()
+  console.log("running a task 10 every minute")
+})
